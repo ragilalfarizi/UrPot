859d3daa500b26b61fe3cec9338a7713858cb2f0
ragilalfarizi <alfariziragill@gmail.com>
2025-05-15 04:16:04 +0700

    test: add test unit of RTC

---
+++ test/embedded/test_rtc/test_rtc.cpp
@@ -0,0 +1,56 @@
+#include <unity.h>
+
+#include "UrPot/rtc.h"
+
+RTC rtc;
+
+void setUp(void) { Serial.begin(115200); }
+
+void tearDown(void) {}
+
+void test_init(void) {
+  esp_err_t ret;
+  ret = rtc.begin();
+  TEST_ASSERT_EQUAL(ret, ESP_OK);
+}
+
+void test_set_and_get_time() {
+  DateTime setTime(2025, 5, 13, 15, 30, 0);
+  DateTime now;
+
+  esp_err_t set_result = rtc.setTimeDate(setTime);
+  TEST_ASSERT_EQUAL_HEX8(ESP_OK, set_result);
+
+  esp_err_t get_result = rtc.getTimeDate(now);
+  TEST_ASSERT_EQUAL_HEX8(ESP_OK, get_result);
+
+  // Allow small difference due to potential clock tick delay
+  TEST_ASSERT_EQUAL(setTime.year(), now.year());
+  TEST_ASSERT_EQUAL(setTime.month(), now.month());
+  TEST_ASSERT_EQUAL(setTime.day(), now.day());
+  TEST_ASSERT_EQUAL(setTime.hour(), now.hour());
+  TEST_ASSERT_EQUAL(setTime.minute(), now.minute());
+
+  // Second may differ due to delay between set/get
+  int secDiff = abs(setTime.second() - now.second());
+  TEST_ASSERT_LESS_THAN_INT(2, secDiff);  // allow up to 1 second drift
+}
+
+int runUnityTests(void) {
+  UNITY_BEGIN();
+  RUN_TEST(test_init);
+  RUN_TEST(test_set_and_get_time);
+  return UNITY_END();
+}
+
+/**
+ * For Arduino framework
+ */
+void setup() {
+  // Wait ~2 seconds before the Unity test runner
+  // establishes connection with a board Serial interface
+  delay(2000);
+
+  runUnityTests();
+}
+void loop() {}
