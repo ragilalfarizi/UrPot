e81a186fb4c0c463b04af48610b58eb6849a250e
ragilalfarizi <alfariziragill@gmail.com>
2025-05-23 17:54:06 +0700

    feat: add BLE audio skeleton (unverified)

--- lib/UrPot/BT_audio.h
+++ lib/UrPot/BT_audio.h
@@ -4,16 +4,39 @@
 #include <esp_err.h>
 
 enum class BTState {
-  DISCONNECTED = 0,
+  DISCONNECTED,
   CONNECTED,
+};
+
+enum class AudioSource {
+  BLUETOOTH,
+  SD_CARD,
 };
 
 class BTAudio {
  public:
-  BTAudio();
+  BTAudio(const char* name);
   ~BTAudio();
 
-  esp_err_t begin();
+  esp_err_t   begin();
+  esp_err_t   playFromBLE();
+  esp_err_t   playFromFile(const char* path);
+  esp_err_t   stop();
+  BTState     getBTState();
+  AudioSource getCurrentSource();
+
+  static const char* TAG;
 
  private:
+  BTState                  _btState;
+  AudioSource              _currentSource;
+  const char*              _deviceName;
+  BluetoothA2DPSink        _a2dp_sink;
+  AudioGeneratorMP3*       _mp3  = nullptr;
+  AudioFileSourceLittleFS* _file = nullptr;
+  AudioOutputI2S*          _out  = nullptr;
+
+  esp_err_t _setupI2S();
+  esp_err_t _setupBT();
+  esp_err_t _setupSDCard();
 };
